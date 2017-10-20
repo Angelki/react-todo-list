3491d64d0d0357c93f876884f46e9b7c857678fa
angelks <angelks4@live.com>
2017-10-20 17:27:07 +0800

    添加部分注释

--- src/Containers/VisibleTodoList.js
+++ src/Containers/VisibleTodoList.js
@@ -7,10 +7,16 @@ import {
 	toggleTodo
 } from '../action';
 
+// 我们将 store 中的数据作为 props 绑定到组件上
 const mapStateToProps = (state, ownProps) => ({
 	todos: state
 });
 
+
+// store.dispatch是view发出action的唯一方法
+// 接受一个Action对象作为参数，将它发送出去
+
+// mapDispatchToProps的功能是，将 action 作为 props 绑定到组件上，也会成为 MyComp 的 props
 const mapDispatchToProps = (dispatch) => ({
 	onTodoClick: (id) => {
 		dispatch(toggleTodo(id));
--- src/store.js
+++ src/store.js
@@ -1,20 +1,21 @@
-import {todos} from './reducer';
-import {createStore} from 'redux';
+import {
+    todos
+} from './reducer';
+import {
+    createStore
+} from 'redux';
 
-const defaultTodos = [
-    {
-        id: 1,
-        content: "学习 React"
-    },
-    {
-        id: 2,
-        content: "学习 Redux"
-    },
-    {
-        id: 3,
-        content: "学习 React-router"
-    },
-];
-
+const defaultTodos = [{
+    id: 1,
+    content: "学习 React"
+}, {
+    id: 2,
+    content: "学习 Redux"
+}, {
+    id: 3,
+    content: "学习 React-router"
+}, ];
+// createStore接受另一个函数作为参数，返回新生成的Store对象，
+// 第二个参数表示初态
 const store = createStore(todos, defaultTodos);
-export default store;                  
+export default store;
